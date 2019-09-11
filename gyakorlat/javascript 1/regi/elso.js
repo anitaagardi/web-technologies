db4fd22bba87d0017757fe928976f08b82937504
anitaagardi <anitaagardi@gmail.com>
2019-09-11 15:31:39 +0200

    new course elements

---
+++ gyakorlat/javascript 1/regi/elso.js
@@ -0,0 +1,20 @@
+<html>
+   <head>
+     
+      <script type ="text/javascript">
+	  
+	  
+	  </script>
+   </head>
+   <body>
+      Adja meg x, y, és z értékét, és számítsa ki a <img src="../bin/keplet.gif"> értékét.
+      <br><br>
+      <form name="fel">
+         X= <input size=3 name="xx"><br><br>
+         Y= <input size=3 name="yy"><br><br>
+         Z= <input size=3 name="zz"><br><br>
+         Eredmény= <input size=10 name="ee"><br><br>
+         <input type=button value="Számítás" onClick="szamol();">
+      </form>
+   </body>
+</html>
